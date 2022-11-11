713d98987b7af30cff4217d26664faf8029c3e63
Isabella  Queiroz <115313447+isabug05@users.noreply.github.com>
2022-11-11 22:24:44 +0000

    Exceções personalizadas criadas.

---
+++ exceptions.py
@@ -0,0 +1,39 @@
+class excecaoGenerica(Exception):
+  pass
+
+
+class UsuarioSenhaVazio(Exception):
+  pass
+
+
+class excecaoLocalNascimentoErrado(Exception):
+  pass
+
+
+class excecaoNomeIndisponivel(Exception):
+  pass
+
+
+class excecaoTamanhoNome(Exception):
+  pass
+
+
+class excecaoTamanhoSenha(Exception):
+  pass
+
+
+class excecaoNenhumAgendamentoSelecionado(Exception):
+  pass
+
+
+class excecaoNomeAgendamentoVazio(Exception):
+  pass
+
+
+class excecaoDataAgendamentoVazia(Exception):
+  pass
+
+
+class excecaoSenhaForte(Exception):
+  pass
+
